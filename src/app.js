8b218ca5576ee5fbe83f5662ff4c147d9dc9f8c0
toshadvidrine <159669056+toshadvidrine@users.noreply.github.com>
2024-05-04 03:18:44 +0000

    html,css,js basics

--- src/app.js
+++ src/app.js
@@ -6,5 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#randomCard").innerHTML = generateRandomCard();
+  });
+
+  const suits = ["spades", "hearts", "diamonds", "clubs"];
+  const values = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "jack",
+    "queen",
+    "king"
+  ];
+
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomValueIndex = Math.floor(Math.random() * values.length);
+
+  const randomSuit = suits[randomSuitIndex];
+  const randomValue = values[randomValueIndex];
+
+  console.log(`Random Card: ${randomValue} of ${randomSuit}`);
+  return `${randomValue} of ${randomSuit}`;
 };
